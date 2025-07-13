922df4446eb21e6fac5a7a5f25bca770b7ae25d1
Banjesh Pandey <banjeshpandey07@gmail.com>
2025-07-13 21:17:54 +0545

    [Arrays] Next Permutation Problem

---
+++ Arrays/NextPermutation.java
@@ -0,0 +1,42 @@
+package Arrays;
+
+import java.util.*;
+
+public class NextPermutation {
+    public static void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+
+        
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+    }
+
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start++, end--);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        nextPermutation(nums);
+        System.out.println(Arrays.toString(nums));  // [1, 3, 2]
+    }
+}
